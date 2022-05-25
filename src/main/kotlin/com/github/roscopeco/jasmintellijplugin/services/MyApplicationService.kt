0c53e3459541e89c7a92627c41861bed2605e861
GitHub Action <action@github.com>
2022-05-25 13:48:37 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/roscopeco/jasmintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.roscopeco.jasmintellijplugin.services
+
+import com.github.roscopeco.jasmintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/roscopeco/jasmintellijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/roscopeco/jasmintellijplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.roscopeco.jasmintellijplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.roscopeco.jasmintellijplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
