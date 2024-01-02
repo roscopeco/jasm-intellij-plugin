69556cf887c5e3de53471f51d624268c0a059da4
Kyle Wood <kyle@denwav.dev>
2024-01-01 23:03:42 -0600

    Update project for IntelliJ 2023.3 compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,41 @@
+import org.jetbrains.changelog.Changelog
 import org.jetbrains.changelog.markdownToHTML
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-fun properties(key: String) = project.findProperty(key).toString()
-
 plugins {
     // Java support
-    id("java")
+    java
     // Kotlin support
-    id("org.jetbrains.kotlin.jvm") version "1.6.10"
+    id("org.jetbrains.kotlin.jvm") version "1.9.0"
     // Gradle IntelliJ Plugin
-    id("org.jetbrains.intellij") version "1.5.2"
+    id("org.jetbrains.intellij") version "1.16.1"
     // Gradle Changelog Plugin
-    id("org.jetbrains.changelog") version "1.3.1"
+    id("org.jetbrains.changelog") version "2.2.0"
     // Gradle Qodana Plugin
-    id("org.jetbrains.qodana") version "0.1.13"
+    id("org.jetbrains.qodana") version "2023.2.1"
 }
 
-group = properties("pluginGroup")
-version = properties("pluginVersion")
+val pluginGroup: String by properties
+val pluginVersion: String by properties
+val pluginName: String by properties
+
+val pluginSinceBuild: String by properties
+val pluginUntilBuild: String by properties
+
+val platformVersion: String by properties
+val platformType: String by properties
+val platformPlugins: String by properties
+
+val javaVersion: String by properties
+val gradleVersion: String by properties
+
+val MutableMap<String, *>.pluginName: String
+    get() = this["pluginName"] as String
+val MutableMap<String, *>.gradleVersion: String
+    get() = this["gradleVersion"] as String
+
+group = pluginGroup
+version = pluginVersion
 
 // Configure project's dependencies
 repositories {
@@ -27,38 +45,36 @@ repositories {
 dependencies {
     implementation("org.antlr:antlr4-intellij-adaptor:0.1")
     implementation("com.roscopeco.jasm:jasm:0.7.0")
+    implementation("org.ow2.asm:asm:9.6")
 }
 
 // Configure Gradle IntelliJ Plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
 intellij {
-    pluginName.set(properties("pluginName"))
-    version.set(properties("platformVersion"))
-    type.set(properties("platformType"))
+    pluginName = properties.pluginName
+    version = platformVersion
+    type = platformType
 
     // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
-    plugins.set(properties("platformPlugins").split(',').map(String::trim).filter(String::isNotEmpty))
+    plugins.set(platformPlugins.split(',').map(String::trim).filter(String::isNotEmpty))
 }
 
 // Configure Gradle Changelog Plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
 changelog {
-    version.set(properties("pluginVersion"))
-    groups.set(emptyList())
+    version = pluginVersion
+    groups = emptyList()
 }
 
 // Configure Gradle Qodana Plugin - read more: https://github.com/JetBrains/gradle-qodana-plugin
 qodana {
-    cachePath.set(projectDir.resolve(".qodana").canonicalPath)
-    reportPath.set(projectDir.resolve("build/reports/inspections").canonicalPath)
-    saveReport.set(true)
-    showReport.set(System.getenv("QODANA_SHOW_REPORT")?.toBoolean() ?: false)
+    cachePath = projectDir.resolve(".qodana").canonicalPath
+    resultsPath = projectDir.resolve("build/reports/inspections").canonicalPath
 }
 
 tasks {
     // Set the JVM compatibility versions
-    properties("javaVersion").let {
+    javaVersion.let {
         withType<JavaCompile> {
-            sourceCompatibility = it
-            targetCompatibility = it
+            options.release = it.toInt()
         }
         withType<KotlinCompile> {
             kotlinOptions.jvmTarget = it
@@ -66,33 +82,31 @@ tasks {
     }
 
     wrapper {
-        gradleVersion = properties("gradleVersion")
+        gradleVersion = project.properties.gradleVersion
     }
 
     patchPluginXml {
-        version.set(properties("pluginVersion"))
-        sinceBuild.set(properties("pluginSinceBuild"))
-        untilBuild.set(properties("pluginUntilBuild"))
+        version = pluginVersion
+        sinceBuild = pluginSinceBuild
+        untilBuild = pluginUntilBuild
 
         // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
-        pluginDescription.set(
-            projectDir.resolve("README.md").readText().lines().run {
-                val start = "<!-- Plugin description -->"
-                val end = "<!-- Plugin description end -->"
+        pluginDescription = projectDir.resolve("README.md").readText().lines().run {
+            val start = "<!-- Plugin description -->"
+            val end = "<!-- Plugin description end -->"
 
-                if (!containsAll(listOf(start, end))) {
-                    throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
-                }
-                subList(indexOf(start) + 1, indexOf(end))
-            }.joinToString("\n").run { markdownToHTML(this) }
-        )
+            if (!containsAll(listOf(start, end))) {
+                throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
+            }
+            subList(indexOf(start) + 1, indexOf(end))
+        }.joinToString("\n").run { markdownToHTML(this) }
 
         // Get the latest available change notes from the changelog file
-        changeNotes.set(provider {
+        changeNotes = provider {
             changelog.run {
-                getOrNull(properties("pluginVersion")) ?: getLatest()
-            }.toHTML()
-        })
+                renderItem(getOrNull(pluginVersion) ?: getLatest(), Changelog.OutputType.HTML)
+            }
+        }
     }
 
     // Configure UI tests plugin
@@ -116,6 +130,6 @@ tasks {
         // pluginVersion is based on the SemVer (https://semver.org) and supports pre-release labels, like 2.1.7-alpha.3
         // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
         // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
-        channels.set(listOf(properties("pluginVersion").split('-').getOrElse(1) { "default" }.split('.').first()))
+        channels.set(listOf(pluginVersion.split('-').getOrElse(1) { "default" }.split('.').first()))
     }
 }
